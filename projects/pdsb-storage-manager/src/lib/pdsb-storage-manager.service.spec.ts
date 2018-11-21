5845642ce3509204ca900d1562797b98626ae1de
Greg <greg.yantz@peelsb.com>
2018-11-21 13:13:20 -0500

    Added a couple of test cases

--- projects/pdsb-storage-manager/src/lib/pdsb-storage-manager.service.spec.ts
+++ projects/pdsb-storage-manager/src/lib/pdsb-storage-manager.service.spec.ts
@@ -3,10 +3,31 @@ import { TestBed } from '@angular/core/testing';
 import { PdsbStorageManagerService } from './pdsb-storage-manager.service';
 
 describe('PdsbStorageManagerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: PdsbStorageManagerService = TestBed.get(PdsbStorageManagerService);
-    expect(service).toBeTruthy();
-  });
+    let service: PdsbStorageManagerService;
+
+    beforeEach(() => TestBed.configureTestingModule({}));
+
+    it('should be created', () => {
+        service = TestBed.get(PdsbStorageManagerService);
+        expect(service).toBeTruthy();
+    });
+
+    it('should be null', () => {
+        const key = 'thisIsNotAStoredValue';
+        expect(service.get(key)).toBeNull();
+    });
+
+    it('should set and get a stored value', () => {
+        const key = 'test';
+        const val = 'test';
+        service.set(key, val, true, false);
+        expect(service.get(key)).toBe(val);
+    });
+
+    it('should remove the stored value', () => {
+        const key = 'test';
+        service.remove(key);
+        expect(service.get(key)).toBeNull();
+    });
 });
